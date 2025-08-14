41e47eb135186c6369ceb84d53c50832a31b2663
unknown <abdulatifsh90@gmail.com>
2025-08-15 04:00:24 +0900

    fix: modify upload images confiiguration

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -3,11 +3,16 @@ import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
+import { graphqlUploadExpress } from 'graphql-upload';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor())
+  app.enableCors({ origin: true, credentials: true });
+	app.use(graphqlUploadExpress({ maxFile: 15000000, maxFiles: 10 }));
+	app.use('/uploads', express.static('./uploads'));
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
